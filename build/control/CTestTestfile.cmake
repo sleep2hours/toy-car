d95f7041feeecbd0de54913bb9577af2d292eea8
sleep2hours <1569171127@qq.com>
2022-03-09 22:47:38 +0800

    add gitignore

--- build/control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lym/code/toy-car/src/control
-# Build directory: /home/lym/code/toy-car/build/control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/img/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/lym/code/toy-car/src/img/include".split(';') if "/home/lym/code/toy-car/src/img/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-limage_tr".split(';') if "-limage_tr" != "" else []
-PROJECT_NAME = "img"
-PROJECT_SPACE_DIR = "/home/lym/code/toy-car/devel"
-PROJECT_VERSION = "0.0.0"
